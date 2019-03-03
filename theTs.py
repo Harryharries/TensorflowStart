ad4157d008eed733cd6bf90e0876bf55ac5b1ae7
zhengyugu <a1021604754@gmail.com>
2019-03-03 13:12:56 -0600

    TSLR

--- theTs.py
+++ theTs.py
@@ -1,2 +1,24 @@
-import tensorflow as ts
+import tensorflow as tf
 import numpy as np
+
+x_data = np.random.rand(100)
+y_data = x_data*0.1+.2
+
+b = tf.Variable(0.)
+k = tf.Variable(0.)
+y = k*x_data + b
+
+loss = tf.reduce_mean(tf.square(y_data - y))
+
+op = tf.train.GradientDescentOptimizer(0.2)
+
+train = op.minimize(loss)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(201):
+        sess.run(train)
+        if step % 20 == 0:
+            print(step,sess.run([k,b]))
